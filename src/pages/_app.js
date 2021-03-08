bbc58db8d603ee17a2d2a537da4a03808268c2a5
Guillo7799 <guillermo.rivera@epn.edu.ec>
2021-03-08 15:02:53 -0500

    Interfaces de inicio y registro

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,30 @@
+import React from 'react';
 import '../styles/globals.css'
 import {AuthProvider} from "@/lib/auth";
 import Navigation from "@/components/Navigation";
+import Head from 'next/head';
+import {CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-      <AuthProvider>
-        <Navigation />
-        <Component {...pageProps} />
-      </AuthProvider>
+    <>
+      <Head>
+        <title>TEACHlearn</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=devide-width"
+        />
+      </Head>
+        <AuthProvider>
+          <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Navigation />
+          <Component {...pageProps} />
+          </ThemeProvider>
+        </AuthProvider>
+    </>
   );
 }
 export default MyApp
