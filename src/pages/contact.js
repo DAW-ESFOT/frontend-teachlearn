58e9fdc4abd311bbb4dfd91f17c5ae7ffe940b1d
Jonathan141999 <jonathan.alquinga@epn.edu.ec>
2021-03-09 13:00:55 -0500

    Pagina de Contacto Completo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,15 +2,16 @@ import React from 'react';
 import styles from '../styles/contact.module.css'
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import Button from "@material-ui/core/Button";
 import Image from 'next/image'
 import {LinkedinOutlined, MailOutlined, WhatsAppOutlined, InstagramOutlined, YoutubeOutlined} from "@ant-design/icons";
+import Link from "@material-ui/core/Link";
 
 const Contact= () =>{
     return (
         <div>
-            <Navigation/>
-
-            <div><h1 style={{textAlign: 'Center'}}>CONTÁCTANOS</h1></div>
+            <div>
+                <h1 style={{textAlign: 'Center'}}>CONTÁCTANOS</h1>
                 <div className={styles.container}>
                     <p style={{margin: '0px'}}>
                         Bienvenido a nuestro sitio web. Estamos contentos de atender tu requerimiento.
@@ -29,7 +30,22 @@ const Contact= () =>{
                             </ul>
                         </div>
                     </div>
+                    <div className={styles.nav}>
+                        <h3>Av. Ladrón de Guevara 253, Quito 170517</h3><h3>-----</h3><h3>Escuela Politécnica Nacional, Universidad pública de Quito</h3>
+                    </div>
+                    <div>
+                        <Image
+                            src="/Mapa.JPG"
+                            alt="Ubicación en el Mapa"
+                            width={800}
+                            height={400}
+                        />
+                    </div>
+                    <Button variant="outlined" color="primary">
+                       <Link href={"https://www.google.com/maps/place/Escuela+Polit%C3%A9cnica+Nacional/@-0.2099005,-78.4899175,16z/data=!4m5!3m4!1s0x91d59a107e1cd44b:0x88a284f66939ed4!8m2!3d-0.2103565!4d-78.4885442"}>Ubicación</Link>
+                    </Button>
                 </div>
+            </div>
             <Footer/>
         </div>
     )
