89c7cbeb101f05dea42d6add8c8c0890c7990092
Jonathan141999 <jonathan.alquinga@epn.edu.ec>
2021-03-17 06:56:57 -0500

    Aplicar bien los botones

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,27 +1,27 @@
-
 const publicRoutes = {
-    LOGIN: "/login",
-    REGISTER: "/registro",
-    CONTACT:"/contact",
-    POLITICS:"/politics",
-    VISION:"/vision-mission",
-    // USERS: "/usuarios",
-    // USERS_ID: `/usuario/:id`,
-    ABOUT: "/about",
-    SUBJECTS:'/materias',
+  LOGIN: "/login",
+  REGISTER: "/registro",
+  CONTACT: "/contact",
+  POLITICS: "/politics",
+  VISION: "/vision-mission",
+  // USERS: "/usuarios",
+  // USERS_ID: `/usuario/:id`,
+  ABOUT: "/about",
+  SUBJECTS: "/materias",
 };
 
 const privateRoutes = {
-    HOME: "/",
-    PROFILE: '/profile',
-    SCHEDULE: '/addTutorial',
-    EDITPROFILE: '/editprofile',
-    ADDTUTORIA:'/addTutorial'
-    // ARTICLE_ID: "/articulo/:id",
+  HOME: "/",
+  PROFILE: "/profile",
+  SCHEDULE: "/addTutorial",
+  EDITPROFILE: "/editprofile",
+  ADDTUTORIA: "/addTutorial",
+  ADDSUBJECT: "/addSubject",
+  // ARTICLE_ID: "/articulo/:id",
 };
 
 const Routes = {
-    ...publicRoutes,
-    ...privateRoutes,
+  ...publicRoutes,
+  ...privateRoutes,
 };
 export default Routes;
